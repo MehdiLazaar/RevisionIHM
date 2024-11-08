dfe63bcfc82667fe0050886a4e81c74eea49690b
Mehdi <mehdilaz109@gmail.com>
2024-11-08 17:15:00 +0100

    Utilisation de JSplitPane et JScrollPane

--- Menu/fenetre1.java
+++ Menu/fenetre1.java
@@ -4,13 +4,16 @@ import java.awt.*;
 import java.awt.event.KeyEvent;
 
 import javax.swing.*;
+import javax.swing.border.LineBorder;
+import javax.swing.border.TitledBorder;
 
 
 public class fenetre1 extends JFrame {
     fenetre1(){
         super("fenetre 1 Menu");
         init();
-        setSize(500,500);
+        init2();
+        pack();
         setDefaultCloseOperation(EXIT_ON_CLOSE);
         setLocationRelativeTo(null);
         setVisible(true);
@@ -50,6 +53,49 @@ public class fenetre1 extends JFrame {
         menuBar.add(aide);
         setJMenuBar(menuBar);
         
+    }
+    void init2(){
+        JPanel panelPrincipal = new JPanel();
+        panelPrincipal.setLayout(new BorderLayout());
+
+        JPanel panelGauche = new JPanel();
+        panelGauche.setLayout(new BoxLayout(panelGauche, BoxLayout.Y_AXIS));
+        JLabel texte1 = new JLabel("Bonjour");
+        JTextArea textArea = new JTextArea(1, 3);
+        textArea.setLineWrap(true);
+        textArea.setWrapStyleWord(true);
+        JScrollPane scrollPaneTexteArea = new JScrollPane(textArea);
+        panelGauche.add(texte1);
+        panelGauche.add(textArea);
+        panelGauche.add(scrollPaneTexteArea);
+
+        JPanel panelDroite = new JPanel();
+        panelDroite.setLayout(new BoxLayout(panelDroite, BoxLayout.Y_AXIS));
+        JLabel texte2 = new JLabel("Choisissez une couleur");
+        JRadioButton bleu = new JRadioButton("Bleu");
+        JRadioButton rouge = new JRadioButton("Rouge");
+        JRadioButton noir = new JRadioButton("Noir");
+        panelDroite.setBorder(new TitledBorder(new LineBorder(Color.BLACK, 2), "Couleur"));
+        ButtonGroup group = new ButtonGroup();
+        group.add(bleu);
+        group.add(rouge);
+        group.add(noir);
+        panelDroite.add(texte2);
+        panelDroite.add(bleu);
+        panelDroite.add(rouge);
+        panelDroite.add(noir);
+        
+
+        JPanel panelMilieu = new JPanel(new FlowLayout());
+        JSplitPane splitPaneMilieu = new JSplitPane(JSplitPane.HORIZONTAL_SPLIT, panelGauche, panelDroite);
+        panelMilieu.add(splitPaneMilieu);
+
+        panelPrincipal.add(panelGauche, BorderLayout.WEST);
+        
+        panelPrincipal.add(panelDroite, BorderLayout.EAST);
+        panelPrincipal.add(splitPaneMilieu, BorderLayout.CENTER);
+
+        add(panelPrincipal);
     }
     public static void main(String[] args) {
         SwingUtilities.invokeLater(fenetre1::new);
